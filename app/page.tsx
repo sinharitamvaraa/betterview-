aa3057cea4a8aa34c885f5215a9db9ed015c73f4
sinharitamvaraa <sinharitamvaraa2020@gmail.com>
2025-11-07 01:32:47 +0530

    fix: properly commented out RealtimeAudio component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SettingsProvider } from "@/lib/settings-provider";
-//import { /*RealtimeAudio*/ } from "@/components/Screenpipe/realtime-audio";
+// import { RealtimeAudio } from "@/components/Screenpipe/realtime-audio";
 import Camera from "@/components/Camera/Camera";
 import { MetricsProvider } from "@/context/MetricsContext";
 import {
@@ -16,7 +16,7 @@ export default function Page() {
       <MetricsProvider>
         <ResizablePanelGroup direction="horizontal">
           <ResizablePanel>
-            <RealtimeAudio />
+            {/* <RealtimeAudio /> */}
           </ResizablePanel>
           <ResizableHandle />
           <ResizablePanel>
